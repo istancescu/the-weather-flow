6a389ce9eff6acd3f50eb9c80c4a3c387a8a2574
Ionut Stancescu <ionut.stancescu@transiris.com>
2020-08-02 18:39:25 +0300

    added colour palette

---
+++ src/utils/colors.ts
@@ -0,0 +1,5 @@
+export const CELTIC_BLUE: string = "#266dd3";
+export const GHOST_WHITE: string = "#fafaff";
+export const CARIBBEAN_GREEN: string = "#00c49a";
+export const PRINCETON_ORANGE: string = "#fa8334";
+export const EERIE_BLACK: string = "#1b2021";
