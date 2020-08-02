68d4f5f7b4b082eab5caa1154fd2a921ca1fbbb0
Ionut Stancescu <ionut.stancescu@transiris.com>
2020-08-02 19:15:38 +0300

    added backg color for homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from "react";
 import NavBar from "../components/NavBar/NavBar";
+import { celtic_blue } from "../utils/colors";
 
 interface Props {}
 
 function Home({  }: Props): ReactElement {
   return (
-    <div>
+    <div style={{ backgroundColor: celtic_blue }}>
       <NavBar />
     </div>
   );
