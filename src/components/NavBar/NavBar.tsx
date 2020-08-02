1656ec12a4118589120515f04318d6dc3c0c0073
istancescu <istancescu@protonmail.com>
2020-08-03 01:18:18 +0300

    added homepage tiles, autowrap

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { ghost_white, celtic_blue } from "../../utils/colors";
+import Logo from "../../assets/sun-line.svg";
 import styles from "./NavBar.module.css";
 
 interface Props {}
@@ -7,9 +7,14 @@ interface Props {}
 export default function NavBar({  }: Props): ReactElement {
   return (
     <head className={styles.container}>
-      <p className={styles.pageTitle}>The Weather Flow</p>
+      <div className={styles.logoContainer}>
+        <p className={styles.pageTitle}>The Weather Flow</p>
+        <img src={Logo} alt="logo" />
+      </div>
       <div>
-        <a className={styles.navBarLink}>Features</a>
+        <a className={styles.navBarLink} href="/">
+          Features
+        </a>
         <a className={styles.navBarLink} href="/">
           Get started
         </a>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 import NavBar from "../components/NavBar/NavBar";
-import { celtic_blue } from "../utils/colors";
 import styles from "./Home.module.css";
+import WeatherTile from "../components/NavBar/WeatherTile";
 
 interface Props {}
 
@@ -10,7 +10,7 @@ function Home({  }: Props): ReactElement {
     <div className={styles.container}>
       <NavBar />
       <div className={styles.subContainer}>
-        <h1 className={styles.title}>Find weather anywhere, at any time</h1>
+        <h1 className={styles.title}>Find weather anywhere, at any time.</h1>
         <p className={styles.description}>
           Tired of the need to carry an umbrella?
         </p>
@@ -21,11 +21,7 @@ function Home({  }: Props): ReactElement {
           <span className={styles.description}>is the perfect solution!</span>
         </div>
         <p className={styles.description}>Simple as it sounds.</p>
-        <div className={styles.weatherContainer}>
-          <p className={styles.weatherTitle}>
-            Find the weather you love, in the location you love.
-          </p>
-        </div>
+        <WeatherTile />
       </div>
     </div>
   );
